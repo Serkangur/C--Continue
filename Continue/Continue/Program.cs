b6171c0b85e575f2cdcf7ccf1f57675dcdb0c530
Serkan <94819174+Serkangur@users.noreply.github.com>
2021-11-23 07:05:15 +0300

    C# Continue
    C# Continue

---
+++ Continue/Continue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 10; a++)
+            {
+                if (a == 8)
+                {
+                    continue;
+
+                }
+                Console.WriteLine("a nın değeri : " + a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
